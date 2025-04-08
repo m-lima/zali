93e065dccba5c9815d81c9b6bbdbd1a97cce40a7
Marcelo Lima <m-lima@users.noreply.github.com>
2025-04-08 17:19:13 +0200

    Add directory fetching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, nix world!");
+const APP_NAME: &str = env!("CARGO_PKG_NAME");
+
+mod path {
+    pub fn data() -> Option<&'static std::path::PathBuf> {
+        static PATH: std::sync::OnceLock<Option<std::path::PathBuf>> = std::sync::OnceLock::new();
+        PATH.get_or_init(|| dirs::data_local_dir().map(|p| p.join(crate::APP_NAME)))
+            .as_ref()
+    }
+}
+
+fn main() -> std::process::ExitCode {
+    let mut args = std::env::args().skip(1);
+    let Some(query) = args.next() else {
+        return std::process::ExitCode::FAILURE;
+    };
+
+    let Ok(a) = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .map(|d| d.as_secs())
+    else {
+        return std::process::ExitCode::FAILURE;
+    };
+
+    print!("{a:?} {query}");
+
+    std::process::ExitCode::SUCCESS
 }
